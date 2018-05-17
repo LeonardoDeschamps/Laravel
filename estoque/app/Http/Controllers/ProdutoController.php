c6b5c4290ee0e13a5132a83415c4c388deaf9eb6
Leonardo Maciel Deschamps <1005440@UNIDAVI.LOCAL>
2018-05-16 21:39:07 -0300

    Primeiro Commit - Construindo aplicações web como um verdadeiro artesão

---
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace estoque\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use estoque\Produto;
+use Request;
+
+class ProdutoController extends Controller {
+
+    public function lista() {
+        $produtos = DB::select('select * from produtos');
+        return view('listagem')->withProdutos($produtos);
+    }
+
+    public function mostra() {
+        $id = Request::input('id', '0');
+        $resposta = DB::select('select * from produtos where id = ?', [$id]);
+        if (empty($resposta)) {
+            return "Esse produto não existe";
+        }
+        return view('detalhes')->with('p', $resposta[0]);
+    }
+
+}
