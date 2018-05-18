6ef63578d3bfd233f4425e1ef1ec97a374618f68
Leonardo Maciel Deschamps <1005440@UNIDAVI.local>
2018-05-17 21:01:19 -0300

    Dando continuidade ao projeto - 8.1 CRIANDO FORMULÁRIO DE NOVOS

--- estoque/app/Http/Controllers/ProdutoController.php
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -3,23 +3,37 @@
 namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use estoque\Produto;
 use Request;
 
 class ProdutoController extends Controller {
 
     public function lista() {
         $produtos = DB::select('select * from produtos');
-        return view('listagem')->withProdutos($produtos);
+        return view('produto.listagem')->withProdutos($produtos);
     }
 
-    public function mostra() {
-        $id = Request::input('id', '0');
+    public function mostra($id) {
         $resposta = DB::select('select * from produtos where id = ?', [$id]);
-        if (empty($resposta)) {
-            return "Esse produto não existe";
-        }
-        return view('detalhes')->with('p', $resposta[0]);
+        $return = !empty($resposta) ? view('produto.detalhes')->with('p', $resposta[0]) : "Esse produto não existe";
+        return $return;
+    }
+
+    public function novo() {
+        return view('produto.formulario');
+    }
+
+    public function adiciona() {
+        $nome = Request::input('nome');
+        $valor = Request::input('valor');
+        $descricao = Request::input('descricao');
+        $quantidade = Request::input('quantidade');
+        DB::table('produtos')->insert([
+            'nome' => $nome,
+            'valor' => $valor,
+            'descricao' => $descricao,
+            'quantidade' => $quantidade
+        ]);
+        return view('produto.adicionado')->with('nome', $nome);
     }
 
 }
--- estoque/routes/web.php
+++ estoque/routes/web.php
@@ -21,4 +21,8 @@ Route::get('/sobre', function () {
 
 Route::get('/produtos', 'ProdutoController@lista');
 
-Route::get('/produtos/mostra', 'ProdutoController@mostra');
+Route::get('/produtos/mostra/{id?}', 'ProdutoController@mostra')->where('id', '[0-9]+');
+
+Route::get('/produtos/novo', 'ProdutoController@novo');
+
+Route::get('/produtos/adiciona', 'ProdutoController@adiciona');
